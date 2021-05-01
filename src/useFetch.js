cccd8bab5227db216b1461b5288aeeaf34c30bd7
Enmanuel Ramirez <enmanuelr7@gmail.com>
2021-05-01 11:44:35 -0300

    useFetch custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setError(null);
+        setData(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
